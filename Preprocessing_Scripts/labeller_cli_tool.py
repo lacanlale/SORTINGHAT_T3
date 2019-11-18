bf72fac0004bb4183f23f1b080ba4dca38610d29
lacanlale <gizmo1398@gmail.com>
2019-11-17 21:18:44 -0800

    Updated file structure

---
+++ Preprocessing_Scripts/labeller_cli_tool.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+
+# NOTE
+# --------------------------------------------------------------------------------------
+# The following is a cli script to help the user interface and manually label the data
+# The workflow of the tool is as follows:
+#   1.) The script presents meaningful feautres [desc stats, column name, random samples]
+#   2.) User is then prompted what label to use
+#   3.) User is then prompted to enter the corresponding reason for being part of that label
+#   4.) Results get recorded in labelled_data.csv
+# --------------------------------------------------------------------------------------
+
+def get_actual():
+    categories = ['Numbers', 'List', 'Datetime', 'Sentence', 'URL', 'Custom Object']
+    count = 1
+    choice = 0
+    while not(choice >= 1 and choice <= 6):
+        choice = 0
+        for category in categories:
+            print(f"{count}.) {category}")
+            count += 1
+        choice = int(input("Reason #: "))
+    return categories[choice-1]
+
+
+def get_reason():
+    reasons = {
+        'a': 'Number value proceeded and/or preceded by a unit string, as well as possibly in-between e.g. ‘100 inches, ‘$15’, ‘-0.12 amps’, ‘~23 feet’, ‘US$ PER 100 LBS’, $5,000,000.00',
+        'b': 'Text corpus that contains a delimiter that differs from standard date formats e.g ‘a,b,c’, ‘word 1; word 2; word 3’, ‘{men|clothing, women|clothing, children|toys etc}’',
+        'c': 'Follows standard numerical formatting seen in applications e.g. ‘12.22.16’, ‘1998/01/01’, ‘30-8-2002’',
+        'd': 'Uses a combination of strings and numbers to convey a date e.g. ‘December 1’, ‘October 30, 2019’, ‘The first of May’',
+        'e': 'Follows standard numerical formatting seen in applications e.g. ‘12:30, ‘1:30:20’, ‘15:00’',
+        'f': 'Uses a combination of strings and numbers to convey a time of day e.g. ‘Half past 12’, ‘12 PM’, ‘9 o’ clock in the morning’',
+        'g': 'Text corpus that does not qualify as a date, timestamp, or URL and conveys significant contextual meaning based on the dataset (title, description, individual information, text excerpt, etc.). This text should also not represent a variable type that may be intended for computer processing e.g. ‘Do schools kill creativity?’, ‘SEE TEXT’, ‘statistics, computer science’',
+        'h': 'Contains a protocol prefix and/or top-level domain suffix with possible additional categorical/product information e.g. ‘google.com’, ‘https://www.ucsd.edu/’, ‘https://www.researchgate.net/blog’',
+        'i': 'Text corpus that may represent a variable type with the intention for computer process e.g. ‘{\'id\': 7, \'name\': \'Funny\', \'count\': 19645}’',
+        'j': 'Multiple numerical values whose context is unrelated to date or time and may require more contextual processing than other numbers e.g. \'(-0.022, 2.2]\', \'(-5.0000, 33.0000)\'',
+        'k': 'Phrases containing words that convey specific geographic information e.g. ‘address : 9415 Campus Point Dr’, ‘mailing address: 345 Airport Rd PO Box 1242 Malta’, ‘MT 59538’',
+        'l': 'Electronic identification data e.g. , ‘email : johndoe@website.com’',
+        'm': 'Descriptive data that should have been classified by an earlier step.',
+        'n': 'Total of NaN values is too high and/or sample items do not convey contextual background of what the data is'
+    }
+    choice = 0
+    while not choice:
+        for key, value in reasons.items():
+            print(f"{key}.) {value}")
+        choice = input("Reason: ")
+    return choice
+
+
+df = pd.read_csv("data/needs_extraction_data/labelled_data.csv")
+row = np.where(df.y_act.isnull())[0][0]
+for num in range(row, len(df)):
+    print("==============================")
+    row_info = df.loc[num]
+    for key, val in row_info.iteritems():
+        print(f"{key}: {val}")
+    print("==============================")
+
+    y_act = get_actual()
+    print()
+    reason = get_reason()
+    row_info.y_act = y_act
+    row_info.Reason = reason
+
+    df.loc[num] = row_info
+
+    print("Would you like to continue? ")
+    inp = input('(Y/n): ').lower()
+    if inp == 'n':
+        break
+
+df.to_csv("data/needs_extraction_data/labelled_data.csv", index=False)
---
+++ Preprocessing_Scripts/rulebased_auto_labeller.py
@@ -0,0 +1,96 @@
+from pandas.api.types import infer_dtype
+import pandas as pd
+import numpy as np
+import re
+
+# NOTE
+# --------------------------------------------------------------------------------------
+# The following is written for our rule-based approach.
+# Rules and order of steps is found in /charts/API_flowchart.png
+# --------------------------------------------------------------------------------------
+def is_url(row, reg, pat):
+    samples = row[6:]
+    for sample in samples:
+        if reg.match(str(sample).strip()) and len(re.sub(pat, '', str(sample).strip())) == 0:
+            return True
+    return False
+
+
+def is_datetime(row):
+    samples = row[6:]
+    for sample in samples:
+        try:
+            _ = pd.Timestamp(sample)
+            return True
+        except ValueError:
+            try:
+                _ = pd.Timestamp(int(sample))
+                return True
+            except ValueError:
+                continue
+    return False
+
+
+def is_email(row, reg):
+    samples = row[6:]
+    for sample in samples:
+        if reg.match(str(sample)):
+            return True
+    return False
+
+
+def is_num(row, reg):
+    samples = row[6:]
+    for sample in samples:
+        try:
+            _ = float(str(sample))
+            return True
+        except ValueError:
+            if reg.match(str(sample).lower()):
+                return True
+    return False
+
+    
+df = pd.read_csv("data/needs_extraction_data/labelled_data.csv")
+identifiers = df[['num_nans', '%_nans', 'mean_word_count',
+       'std_dev_word_count', 'has_delimiters', 'sample_1', 'sample_2',
+       'sample_3', 'sample_4', 'sample_5']].rename(columns={'%_nans' : 'perc_nans'}).fillna('NANVAL!')
+
+rulebook_preds = []
+
+url_pat = r"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
+url_reg = re.compile(url_pat)
+
+list_pat =  r"((\d|\w|\')+(,|>|;|:|\-|`\.|\||\*){1}\s?(\d|\w|\')+){2,}"
+list_reg = re.compile(list_pat)
+
+num_pat = r"([\$|\w]?(\d|\d\.\d|\d\,\d)+\s?[\$|\w]?)"
+num_reg = re.compile(num_pat)
+
+email_pat = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
+email_reg = re.compile(email_pat)
+
+for row in identifiers.itertuples():
+    if float(row.perc_nans) >= 0.90:
+        rulebook_preds.append('Custom Object')
+    elif float(row.mean_word_count) <= 2.0:
+        if is_datetime(row):
+            rulebook_preds.append('Datetime')
+        elif is_email(row, email_reg):
+            rulebook_preds.append('Custom Object')
+        elif is_url(row, url_reg, url_pat):
+            rulebook_preds.append('URL')
+        elif is_num(row, num_reg):
+            rulebook_preds.append('Numbers')
+        else:
+            rulebook_preds.append('Custom Object')
+    elif float(row.std_dev_word_count) < 10.0:
+        if row.has_delimiters == True:
+            rulebook_preds.append('List')
+        else:
+            rulebook_preds.append('Custom Object')
+    else:
+        rulebook_preds.append('Sentence')
+
+df['y_pred'] = rulebook_preds 
+df.to_csv('data/needs_extraction_data/rulebook_labelled.csv', index=False)
